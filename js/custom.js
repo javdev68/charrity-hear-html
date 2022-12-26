763b5dcc98bbdc7e63d3a9038673bf46bbd6409e
javdev68 <j.ghanbari68@yahoo.com>
2022-12-26 18:29:21 +0330

    run slick slider and styles

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function ($){
+
+    /* slick slider */
+    $('.slider__col').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        infinite: true,
+        speed: 2000,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+
+});
